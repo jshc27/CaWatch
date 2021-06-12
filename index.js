ac848cb47c6665524160e7ff486da1adfdcf68e0
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-06-12 15:01:54 -0500

    addin changes: button reset and functions

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-function suma(value) {
-    document.getElementById('result').innerHTML = value;
-}
-
-function resta(value) {
-    document.getElementById('result').innerHTML = value;
-}
-
-function division(value) {
-    document.getElementById('result').innerHTML = value;
-}
-
-function multiplicacion(value) {
-    document.getElementById('result').innerHTML = value;
-}
-
-function numero(value) {
-    document.getElementById('result').innerHTML = value;
-}
-
-
-function igual(value) {
-    document.getElementById('result').innerHTML = value;
+function inicio() {
+    //Variables
+    var result = document.getElementById('result');
+    var igual = document.getElementById('igual');
+    var suma = document.getElementById('suma');
+    var point = document.getElementById('point');
+    var number0 = document.getElementById('number0');
+    var resta = document.getElementById('resta');
+    var number1 = document.getElementById('number1');
+    var number2 = document.getElementById('number2');
+    var number3 = document.getElementById('number3');
+    var multiplicacion = document.getElementById('multiplicacion');
+    var number4 = document.getElementById('number4');
+    var number5 = document.getElementById('number5');
+    var number6 = document.getElementById('number6');
+    var division = document.getElementById('division');
+    var number9 = document.getElementById('number9');
+    var number8 = document.getElementById('number8');
+    var number7 = document.getElementById('number7');
 }
