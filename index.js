bbabcea4d2ca06295f7573f450ca03bd051d3086
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-06-12 15:23:33 -0500

    add changes: changing structure

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-function inicio() {
+
+
+
+function init() {
+
     //Variables
     var result = document.getElementById('result');
     var igual = document.getElementById('igual');
     var suma = document.getElementById('suma');
     var point = document.getElementById('point');
-    var number0 = document.getElementById('number0');
+    var 0 = document.getElementById('0');
     var resta = document.getElementById('resta');
-    var number1 = document.getElementById('number1');
+    var 1 = document.getElementById('1');
     var number2 = document.getElementById('number2');
     var number3 = document.getElementById('number3');
     var multiplicacion = document.getElementById('multiplicacion');
@@ -17,4 +21,60 @@ function inicio() {
     var number9 = document.getElementById('number9');
     var number8 = document.getElementById('number8');
     var number7 = document.getElementById('number7');
+
+    //Eventos
+
+    
+    number1.onclick = function(e) {
+        result.textContent = result.textContent + "1";
+    }
+
+    number2.onclick = function(e) {
+        result.textContent = result.textContent + "2";
+    }
+
+    number3.onclick = function(e) {
+        result.textContent = result.textContent + "3";
+    }
+    
+    number4.onclick = function(e) {
+        result.textContent = result.textContent + "4";
+    }
+    
+    number5.onclick = function(e) {
+        result.textContent = result.textContent + "5";
+    }
+    
+    number6.onclick = function(e) {
+        result.textContent = result.textContent + "6";
+    }
+    
+    number7.onclick = function(e) {
+        result.textContent = result.textContent + "7";
+    }
+    
+    number8.onclick = function(e) {
+        result.textContent = result.textContent + "8";
+    }
+    
+    number9.onclick = function(e) {
+        result.textContent = result.textContent + "9";
+    }
+
+    number0.onclick = function(e) {
+        result.textContent = result.textContent + "0";
+    }
+
+    reset.onclick = function(e) {
+       resetear();
+    }
+
+    suma.onclick = function(e) {
+
+     }
 }
+
+
+
+    
+    
