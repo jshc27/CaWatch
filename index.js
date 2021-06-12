c7fd64599500e36a794a31f994e84ea7ae00063c
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-06-12 13:35:50 -0500

    add changes: operations

---
+++ index.js
@@ -0,0 +1,24 @@
+function suma(value) {
+    document.getElementById('result').innerHTML = value;
+}
+
+function resta(value) {
+    document.getElementById('result').innerHTML = value;
+}
+
+function division(value) {
+    document.getElementById('result').innerHTML = value;
+}
+
+function multiplicacion(value) {
+    document.getElementById('result').innerHTML = value;
+}
+
+function numero(value) {
+    document.getElementById('result').innerHTML = value;
+}
+
+
+function igual(value) {
+    document.getElementById('result').innerHTML = value;
+}
