08e027e7fcf340c570ff7f939c4f8846a50a7724
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-06-17 17:06:18 -0500

    adding changes: file JS events

--- index.js
+++ index.js
@@ -1,77 +1,57 @@
-
-
-
 function init() {
-
     //Variables
-    var result = document.getElementById('result');
-    var igual = document.getElementById('igual');
-    var suma = document.getElementById('suma');
-    var point = document.getElementById('point');
-    var 0 = document.getElementById('0');
-    var resta = document.getElementById('resta');
-    var 1 = document.getElementById('1');
-    var number2 = document.getElementById('number2');
-    var number3 = document.getElementById('number3');
-    var multiplicacion = document.getElementById('multiplicacion');
-    var number4 = document.getElementById('number4');
-    var number5 = document.getElementById('number5');
-    var number6 = document.getElementById('number6');
-    var division = document.getElementById('division');
-    var number9 = document.getElementById('number9');
-    var number8 = document.getElementById('number8');
-    var number7 = document.getElementById('number7');
-
+    var result = document.getElementById("result");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var division = document.getElementById("division");
+    var seis = document.getElementById("seis");
+    var cinco = document.getElementById("cinco");
+    var cuatro = document.getElementById("cuatro");
+    var multiplicacion = document.getElementById("multiplicacion");
+    var tres = document.getElementById("tres");
+    var dos = document.getElementById("dos");
+    var uno = document.getElementById("uno");
+    var resta = document.getElementById("resta");
+    var cero = document.getElementById("cero");
+    var punto = document.getElementById("punto");
+    var mas = document.getElementById("mas");
+    var igual = document.getElementById("igual");
+    var reset = document.getElementById("reset");
+    
     //Eventos
 
-    
-    number1.onclick = function(e) {
+    uno.onclick = function(e) {
         result.textContent = result.textContent + "1";
     }
-
-    number2.onclick = function(e) {
+    dos.onclick = function(e) {
         result.textContent = result.textContent + "2";
     }
-
-    number3.onclick = function(e) {
+    tres.onclick = function(e) {
         result.textContent = result.textContent + "3";
     }
-    
-    number4.onclick = function(e) {
+    cuatro.onclick = function(e) {
         result.textContent = result.textContent + "4";
     }
-    
-    number5.onclick = function(e) {
+    cinco.onclick = function(e) {
         result.textContent = result.textContent + "5";
     }
-    
-    number6.onclick = function(e) {
+    seis.onclick = function(e) {
         result.textContent = result.textContent + "6";
     }
-    
-    number7.onclick = function(e) {
+    siete.onclick = function(e) {
         result.textContent = result.textContent + "7";
     }
-    
-    number8.onclick = function(e) {
+    ocho.onclick = function(e) {
         result.textContent = result.textContent + "8";
     }
-    
-    number9.onclick = function(e) {
+    nueve.onclick = function(e) {
         result.textContent = result.textContent + "9";
     }
-
-    number0.onclick = function(e) {
+    cero.onclick = function(e) {
         result.textContent = result.textContent + "0";
     }
 
-    reset.onclick = function(e) {
-       resetear();
-    }
-
-    suma.onclick = function(e) {
-
-     }
 }
 
 
